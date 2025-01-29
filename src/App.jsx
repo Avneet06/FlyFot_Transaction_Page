24b8645f7bfe8fe56172b0591ce8bd152337b647
Avneet Singh <avneet.ch02@gmail.com>
2025-01-29 16:51:38 +0530

    changed name for a file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 import Navbar from './components/Navbar'
 import { BrowserRouter,Route,Routes } from 'react-router-dom'
-import Create from './components/create'
+import Create from './components/Add'
 import Read from './components/Read'
 import View from './components/View'
 import Settlement from './components/Settlement'
